1b543459ba7702fb7c0039d0ebb2bfe5de550d60
Mingau <enzosantiagosrv1245@gmail.com>
2025-08-31 20:06:11 +0000

    e

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ const io = new Server(server);
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname, public));
+const path = require("path");
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 
 const USERS_FILE = path.join(__dirname, "users.json");
